7c72cbf2c0a647f70aeeae0da38417c3171e009a
Landon Petzoldt <clurdish@gmail.com>
2016-04-06 15:17:49 -0500

    Add code

---
+++ VS_Test1/main.cpp
@@ -0,0 +1,25 @@
+
+//Landon Petzoldt
+
+#include <iostream>
+#include <vector>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v1{ 1, 2, 3, 4 };
+
+	for (int i : v1)
+	{
+		cout << i << " ";
+	}
+
+	vector<int> v2{ v1 };
+	for (int i : v2)
+		cout << i;
+
+	_getch();
+	return 0;
+}
